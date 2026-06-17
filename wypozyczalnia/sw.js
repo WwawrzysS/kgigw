140fbe22184bb99dd773ca784f5d9fab1dc48ec2
WwawrzysS <wawrzysdom@gmail.com>
2026-06-17 15:45:54 +0200

    Add files via upload

--- wypozyczalnia/sw.js
+++ wypozyczalnia/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "kgigw-wypozyczalnia-folder-v20260604-06";
+const CACHE_NAME = "kgigw-wypozyczalnia-folder-v20260604-07";
 const APP_SHELL = [
   "/kgigw/wypozyczalnia/",
   "/kgigw/wypozyczalnia/index.html",
